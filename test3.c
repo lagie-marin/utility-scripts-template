2402f8cce8e73d78f61f364e364a85e42fc92589
lagie-marin <marin.lagie@epitech.eu>
2023-12-26 14:32:52 +0100

    [AUTO-MOVE]move

---
+++ test3.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2023
+** utility-scripts-template
+** File description:
+** test3.c
+*/
+
+int test3(void)
+{
+    return 0;
+}
+
+int main(void)
+{
+    test3();
+    return (0);
+}
