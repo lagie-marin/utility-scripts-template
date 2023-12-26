c4a5d2b0a0c90a12d1e4d0d34b689dc1441df60d
lagie-marin <marin.lagie@epitech.eu>
2023-12-26 15:05:01 +0100

    test1.c

---
+++ test1.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** utility-scripts-template
+** File description:
+** test1.c
+*/
+
+#include "include/my.h"
+
+int test1(void)
+{
+    return 0;
+}
+
+int main(void)
+{
+    test1();
+    return (0);
+}
---
+++ test2.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2023
+** utility-scripts-template
+** File description:
+** test2.c
+*/
+
+#include "include/my.h"
+
+int test2(void)
+{
+    return 0;
+}
+
+int main(void)
+{
+    test2();
+    return (0);
+}
