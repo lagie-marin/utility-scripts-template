a084621da80d631a98b068939b5417b530f36535
lagie-marin <marin.lagie@epitech.eu>
2024-01-24 12:44:59 +0100

    [FIX]ajoute \t devant le fichier dans le makefile

---
+++ new.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2024
+** utility-scripts-template
+** File description:
+** new.c
+*/
+
+int new(void)
+{
+    return 0;
+}
+
+int main(void)
+{
+    new();
+    return (0);
+}
